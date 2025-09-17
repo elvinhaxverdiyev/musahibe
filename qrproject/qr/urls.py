632bc7460a9c9e47a0a1121d63bfcafcb8bbf6ef
Elvin <elvinhaxverdiyev777@gamil.com>
2025-09-17 23:00:30 +0400

    debg

--- qrproject/qr/urls.py
+++ qrproject/qr/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.create_product, name="create-product"),
-    path("01/<str:gtin>/", views.product_detail, name="product-detail"),
+    path("product/<str:gtin>/", views.product_detail, name="product-detail"),
+
 ]
