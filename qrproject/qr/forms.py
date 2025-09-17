a76d67aff549bcd9c90686a8ba36c9846e49971a
Elvin <elvinhaxverdiyev777@gamil.com>
2025-09-17 23:14:55 +0400

    debg

--- qrproject/qr/forms.py
+++ qrproject/qr/forms.py
@@ -4,4 +4,15 @@ from .models import Product
 class ProductForm(forms.ModelForm):
     class Meta:
         model = Product
-        fields = ['gtin', 'name', 'year', 'product_type', 'volume', 'producer', 'description', 'ingredients', 'image']
+        fields = '__all__'
+        labels = {
+            'gtin': 'GTIN',
+            'name': 'Ad',
+            'year': 'İl',
+            'product_type': 'Növ',
+            'volume': 'Həcm',
+            'producer': 'İstehsalçı',
+            'description': 'Təsvir',
+            'ingredients': 'Tərkibi',
+            'image': 'Şəkil',  
+        }
