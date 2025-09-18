1581787fb14a5e8a5d4486ccdfe77ee7a881da22
Elvin <elvinhaxverdiyev777@gamil.com>
2025-09-18 20:07:34 +0400

    debug

--- qrproject/qr/views.py
+++ qrproject/qr/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Product
 from .forms import ProductForm
+from barcode import Code128
+from barcode.writer import ImageWriter
 import qrcode
 from io import BytesIO
 from django.http import HttpResponse
 from django.conf import settings
 import os
+
 
 # AWS public IP və ya domain
 AWS_PUBLIC_HOST = "http://16.170.243.177:8000"
@@ -35,11 +38,29 @@ def create_product(request):
     return render(request, "product_form.html", {"form": form})
 
 
-# Telefon scan edəndə açılacaq HTML səhifə
 def product_detail(request, gtin):
     product = get_object_or_404(Product, gtin=gtin)
-    return render(request, "product_detail.html", {"product": product})
 
+    # Barkod mətni (GS1-də Application Identifier 01 + GTIN)
+    barcode_text = f"01{product.gtin}"
+
+    # Barkod fayl adı və yolu
+    barcode_filename = f"barcode_{product.gtin}.png"
+    barcode_path = os.path.join(settings.MEDIA_ROOT, barcode_filename)
+
+    # Əgər barkod şəkli yoxdursa, yarat
+    if not os.path.exists(barcode_path):
+        Code128(barcode_text, writer=ImageWriter()).save(
+            barcode_path.replace(".png", "")
+        )
+
+    # Template üçün URL
+    barcode_url = f"{settings.MEDIA_URL}{barcode_filename}"
+
+    return render(request, "product_detail.html", {
+        "product": product,
+        "barcode_url": barcode_url
+    })
 
 # QR kodu browserdə görmək üçün (optional)
 def create_qr(request, gtin):
