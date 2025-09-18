a95ca3b4ab283253cc8d66bdc8613be311d8a9c8
Elvin <elvinhaxverdiyev777@gamil.com>
2025-09-18 21:05:31 +0400

    debug

--- qrproject/qr/admin.py
+++ qrproject/qr/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Product
 
-# Register your models here.
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'gtin', 'year', 'product_type', 'volume', 'producer')
+    search_fields = ('name', 'gtin', 'producer')
+    list_filter = ('year', 'product_type', 'producer')
+    readonly_fields = ('gtin',) 
