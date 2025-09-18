68d0c23f6f429bec879e40b8d6443dbf2094b77d
Elvin <elvinhaxverdiyev777@gamil.com>
2025-09-18 20:32:30 +0400

    debug

--- qrproject/qr/models.py
+++ qrproject/qr/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class Product(models.Model):
-    gtin = models.CharField(max_length=14, unique=True)  # məhsulun unikal kodu
+    gtin = models.CharField(max_length=14, unique=True) 
     name = models.CharField(max_length=255)
     year = models.CharField(max_length=10, blank=True, null=True)
     product_type = models.CharField(max_length=50)
@@ -9,8 +9,7 @@ class Product(models.Model):
     producer = models.CharField(max_length=255)
     description = models.TextField()
     ingredients = models.TextField()
-    image = models.ImageField(upload_to='media/', blank=True, null=True)  # Yeni sahə
-
+    image = models.ImageField(upload_to='media/', blank=True, null=True)  
 
     def __str__(self):
         return f"{self.name} ({self.gtin})"
--- qrproject/qr/views.py
+++ qrproject/qr/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Product
-from .forms import ProductForm
 from barcode import Code128
 from barcode.writer import ImageWriter
 import qrcode
@@ -9,21 +7,20 @@ from django.http import HttpResponse
 from django.conf import settings
 import os
 
+from .models import Product
+from .forms import ProductForm
 
-# AWS public IP və ya domain
+
 AWS_PUBLIC_HOST = "http://16.170.243.177:8000"
 
-# Məhsul əlavə etmək və QR yaratmaq
 def create_product(request):
     if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid():
             product = form.save()
 
-            # QR URL-i: sadəcə /01/<gtin>/ və AWS public IP ilə
             url = f"{AWS_PUBLIC_HOST}/01/{product.gtin}/"
 
-            # QR kod şəkli yarat
             qr = qrcode.make(url)
             qr_path = os.path.join(settings.MEDIA_ROOT, f"qr_{product.gtin}.png")
             qr.save(qr_path)
@@ -41,20 +38,17 @@ def create_product(request):
 def product_detail(request, gtin):
     product = get_object_or_404(Product, gtin=gtin)
 
-    # Barkod mətni (GS1-də Application Identifier 01 + GTIN)
     barcode_text = f"01{product.gtin}"
 
-    # Barkod fayl adı və yolu
     barcode_filename = f"barcode_{product.gtin}.png"
     barcode_path = os.path.join(settings.MEDIA_ROOT, barcode_filename)
 
-    # Əgər barkod şəkli yoxdursa, yarat
     if not os.path.exists(barcode_path):
         Code128(barcode_text, writer=ImageWriter()).save(
             barcode_path.replace(".png", "")
         )
 
-    # Template üçün URL
+    
     barcode_url = f"{settings.MEDIA_URL}{barcode_filename}"
 
     return render(request, "product_detail.html", {
@@ -62,7 +56,6 @@ def product_detail(request, gtin):
         "barcode_url": barcode_url
     })
 
-# QR kodu browserdə görmək üçün (optional)
 def create_qr(request, gtin):
     url = f"{AWS_PUBLIC_HOST}/01/{gtin}/"
     img = qrcode.make(url)
